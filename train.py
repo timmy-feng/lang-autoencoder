a5ca99d4f58c72fa3cb789138819bc31829b70e9
timmy-feng <timf01089@gmail.com>
2023-11-21 14:59:15 -0500

    CUDA support

--- train.py
+++ train.py
@@ -19,6 +19,8 @@ config = yaml.safe_load(open(args.config, 'r'))
 input_vocab_size = config['lang']['input']['vocab_size']
 output_vocab_size = config['lang']['output']['vocab_size']
 
+device = torch.device('cuda:0')
+
 src_embed = nn.Linear(input_vocab_size, config['model']['d_model'], bias = False)
 conlang_embed = nn.Linear(output_vocab_size, config['model']['d_model'], bias = False)
 
@@ -54,14 +56,20 @@ optimizer = optim.Adam(model.parameters(), lr = config['train']['lr'])
 
 train_dataset = torch.load(config['dataset']['train'])
 train_loader = DataLoader(train_dataset, batch_size = config['train']['batch_size'], shuffle = True)
+model.to(device)
 
 for epoch in range(config['train']['epochs']):
     print(f'Epoch {epoch + 1}:')
-    for src, in tqdm(train_loader):
-        src = F.one_hot(src, input_vocab_size).float()
+    progress_bar = tqdm(total = len(train_loader), position = 0)
+    train_loss_log = tqdm(total = 0, position = 1, bar_format = '{desc}')
+
+    for src, in train_loader:
+        src = F.one_hot(src, input_vocab_size).float().to(device)
         output = model(src)
         loss = criterion(output, src)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        print(f'Train Loss: {loss.item()}')
+
+        train_loss_log.set_description_str(f'Train loss: {loss.item():.4f}')
+        progress_bar.update(1)
