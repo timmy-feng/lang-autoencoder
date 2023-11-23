879172294eb2c1b09c4e4879592099bc47433177
timmy-feng <timf01089@gmail.com>
2023-11-22 22:31:31 -0500

    save models on completion

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 
 import argparse
 import yaml
+import os
 from tqdm import tqdm
 
 from model import TransformerEncoder, Transformer, Autoencoder
@@ -65,7 +66,6 @@ torch.nn.utils.clip_grad_norm_(model.parameters(), config['train']['clip_grad_no
 for epoch in range(config['train']['epochs']):
     progress_bar = tqdm(total = len(train_loader), position = 0)
     train_loss_log = tqdm(total = 0, position = 1, bar_format = '{desc}')
-    val_loss_log = tqdm(total = 0, position = 2, bar_format = '{desc}')
 
     progress_bar.write(f'Epoch {epoch + 1}')
 
@@ -83,11 +83,14 @@ for epoch in range(config['train']['epochs']):
         optimizer.step()
         optimizer.zero_grad()
 
-        accuracy = ((torch.argmax(output, dim = 1) == src).sum() - (src == 0).sum()) / (src != 0).sum()
+        accuracy = ((torch.argmax(output, dim = 1) == src) * (src != 0)).sum() / (src != 0).sum()
 
         train_loss_log.set_description_str(f'Train loss: {loss.item():.4f}, '
             f'Train accuracy: {accuracy:.4f}')
         progress_bar.update(1)
+
+    progress_bar.close()
+    train_loss_log.close()
 
     total_loss, total_samples, total_matches, total_tokens = 0, 0, 0, 0
     for src, in val_loader:
@@ -102,7 +105,15 @@ for epoch in range(config['train']['epochs']):
 
         total_loss += loss.detach().item() * src.size(0)
         total_samples += src.size(0)
-        total_matches += (torch.argmax(output, dim = 1) == src).sum() - (src == 0).sum()
+        total_matches += ((torch.argmax(output, dim = 1) == src) * (src != 0)).sum()
         total_tokens += (src != 0).sum()
-    val_loss_log.set_description_str(f'Val loss: {(total_loss / total_samples):.4f}, '
+
+    print(f'Val loss: {(total_loss / total_samples):.4f}, '
         f'Val accuracy: {(total_matches / total_tokens):.4f}')
+    print()
+    
+save_path = config['train']['path']
+save_dir = os.path.dirname(save_path)
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+print(f'Model saved to {save_path}')
